40ad382b0ef2f959c85d2506629a88381388ee5e
amith <amith@DESKTOP-4EPK2KM>
2022-09-26 22:30:48 -0500

    Lab4 Initial commit

---
+++ src/main/java/edu/iit/sat/itmd4515/asatyanarayan1/domain/Patient.java
@@ -0,0 +1,181 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.iit.sat.itmd4515.asatyanarayan1.domain;
+
+import java.time.LocalDate;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+/**
+ *
+ * @author amith
+ */
+@Entity
+public class Patient {
+
+    
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name="patient_name", nullable=false,unique=true)
+    private String name;
+    @Enumerated(EnumType.STRING)
+    private PatientGender gender;
+
+    public Patient(String name, PatientGender gender, LocalDate dob, String address) {
+        this.name = name;
+        this.gender = gender;
+        this.dob = dob;
+        this.address = address;
+    }
+
+    public Patient() {
+    }
+    
+    
+    
+
+    /**
+     * Get the value of gender
+     *
+     * @return the value of gender
+     */
+    public PatientGender getGender() {
+        return gender;
+    }
+
+    /**
+     * Set the value of gender
+     *
+     * @param gender new value of gender
+     */
+    public void setGender(PatientGender gender) {
+        this.gender = gender;
+    }
+
+    
+
+    /**
+     * Get the value of id
+     *
+     * @return the value of id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @param id new value of id
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    
+    private LocalDate dob;
+    
+    private String address;
+
+    /**
+     * Get the value of address
+     *
+     * @return the value of address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * Set the value of address
+     *
+     * @param address new value of address
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+   
+    /**
+     * Get the value of dob
+     *
+     * @return the value of dob
+     */
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    /**
+     * Set the value of dob
+     *
+     * @param dob new value of dob
+     */
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+ 
+
+    
+
+    /**
+     * Get the value of name
+     *
+     * @return the value of name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @param name new value of name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" + "id=" + id + ", name=" + name + ", dob=" + dob + ", address=" + address + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 17 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        if (this == that) {
+            return true;
+        }
+        if (that == null) {
+            return false;
+        }
+        if (getClass() != that.getClass()) {
+            return false;
+        }
+        final Patient other = (Patient) that;
+        if((this.id == null) || (other.id==null)){
+            return false;
+        }
+        return Objects.equals(this.id, other.id);
+    }
+
+   
+
+}
